e201beb060d9350401787f5d1eb5890ce29955f7
Anthony Ajjawi <ajjawi_amer@hotmail.com>
2025-03-19 16:17:26 -0400

    accessed the close task form btn, update-or-delete-task, and cancel btn

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //access task-form, close-dialog, open-task-form btn
 
-let taskForm = document.getElementById("taskForm");
-let close_Dialog = document.getElementById("confirm-close-dialog");
-let open_Task_Btn = document.getElementById("open-task-form-btn");
+const taskForm = document.getElementById("taskForm");
+const close_Dialog = document.getElementById("confirm-close-dialog");
+const open_Task_Btn = document.getElementById("open-task-form-btn");
+
+
+//access close-task-form-btn, add-or-update-task-btn, cancel-btn
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("update-or-delete-task");
+const cancelBtn = document.getElementById("cancel-btn");

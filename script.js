106c47d7e238e547c63759bf1c88d0a7156d8323
Anthony Ajjawi <ajjawi_amer@hotmail.com>
2025-03-19 16:03:47 -0400

    created variables for accessing the task-form, open-task form, and close-dialog

---
+++ script.js
@@ -0,0 +1,5 @@
+//access task-form, close-dialog, open-task-form btn
+
+let taskForm = document.getElementById("taskForm");
+let close_Dialog = document.getElementById("confirm-close-dialog");
+let open_Task_Btn = document.getElementById("open-task-form-btn");

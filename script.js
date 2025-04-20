6bbc833f4b8390a9de4d84e881303133347b677e
Anthony Ajjawi <ajjawi_amer@hotmail.com>
2025-04-20 10:56:13 -0400

    interpolated the p strings

--- script.js
+++ script.js
@@ -45,7 +45,11 @@ taskForm.addEventListener("submit", (e) => {
   }
 
   taskData.forEach(({id, title, date, description}) => {
-      tasksContainer.innerHTML += `<div id="${id}" class="task"></div>`;
-  });
-
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+        <p><strong>Title:</strong>${title}</p>
+        </div>
+      `
+    }
+  );
 });

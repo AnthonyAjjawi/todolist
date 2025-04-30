25d8b9b018e88ecfe7239c60d1fb28a9c85ff5b6
Anthony Ajjawi <ajjawi_amer@hotmail.com>
2025-04-30 10:50:30 -0400

    laid out reset function

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
+const reset = () => {
+  
+}
 
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
